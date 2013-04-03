d7fcfe5e8b8318e8327b127a09fc8ece4c7043ec
Anders Damsgaard Christensen <adc@cs.au.dk>
2013-04-03 16:05:12 +0200

    added scripts to clone git repos

---
+++ .vim/bundle/clone-repos.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+git clone git://github.com/Lokaltog/vim-powerline.git
+git clone git://github.com/scrooloose/nerdtree.git
+git clone git://github.com/scrooloose/syntastic.git
+git clone git://github.com/kien/ctrlp.vim.git
+git clone git://github.com/klen/python-mode.git
