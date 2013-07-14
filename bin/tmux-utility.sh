a1d0d314e4c779d0754ee448f98062b97337456e
Anders Damsgaard <adc@cs.au.dk>
2013-07-14 21:25:11 +0200

    echo corresponds to session name

--- bin/tmux-utility.sh
+++ bin/tmux-utility.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-echo "starting utility tmux session"
-
 SESSION=utility
+echo "starting $SESSION tmux session"
 
 tmux has-session -t $SESSION
 if [ $? != 0 ]
@@ -10,13 +9,15 @@ then
     tmux new-session -s $SESSION -n misc -d
 
     tmux send-keys -t $SESSION 'mutt' C-m   # process in first pane
-    tmux split-window -h -p 55 -t $SESSION  # horizontal split
+    tmux split-window -h -p 60 -t $SESSION  # horizontal split
     tmux select-pane -t $SESSION:1.1        # select first pane
     tmux split-window -v -p 50 -t $SESSION  # create a pane below first pane
     tmux send-keys -t $SESSION 'ncmpc' C-m  # process in second pane
     tmux split-window -v -p 50 -t $SESSION  # create a pane below second pane
     tmux send-keys -t $SESSION 'irssi.sh' C-m  # process in third pane
+    tmux select-pane -t $SESSION:1.4        # select fourth pane
+    tmux split-window -v -p 10 -t $SESSION  # split right pane into two
+    #tmux send-keys -t $SESSION 'uu' C-m     # start update
     tmux select-pane -t $SESSION:1.1        # select first pane
-    #tmux select-pane -t $SESSION:1.4        # select fourth pane
 fi
 tmux attach -t $SESSION
