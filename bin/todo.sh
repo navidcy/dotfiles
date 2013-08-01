85c7b365a68ce3bf52fb377cf0910e4e9c779377
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-08-01 12:29:36 +0200

    added port number

--- bin/todo.sh
+++ bin/todo.sh
@@ -8,7 +8,7 @@ VIMVERS=`vim --version | head -n 1 | awk '{print $5 }'`
 REQVIMVERS="6.0"
 
 if [ `echo $VIMVERS'>='$REQVIMVERS | bc -l` -eq "1" ]; then
-    vim scp://adc@susanoganders.dk//home/adc/doc/todo.rst
+    vim scp://adc@susanoganders.dk:12344//home/adc/doc/todo.rst
 else
     echo "The installed Vim version is too old ($VIMVERS). This script needs version $REQVIMVERS or later."
     exit 1;
