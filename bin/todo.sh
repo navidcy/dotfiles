27cc0bab83081651bac69d0312a79038a53e0893
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-08-02 10:31:32 +0200

    Todo file now edited in server vim session inside screen

--- bin/todo.sh
+++ bin/todo.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
 
-# Edit central todo-file by ssh and Vim.
-# Requires vim 6.x or later
-
-# Check Vim version
-VIMVERS=`vim --version | head -n 1 | awk '{print $5 }'`
-REQVIMVERS="6.0"
-
-if [ `echo $VIMVERS'>='$REQVIMVERS | bc -l` -eq "1" ]; then
-    vim scp://adc@susanoganders.dk:12344//home/adc/doc/todo.rst
-else
-    echo "The installed Vim version is too old ($VIMVERS). This script needs version $REQVIMVERS or later."
-    exit 1;
-fi
+# Edit central todo-file by ssh
+ssh -t -p 12344 adc@susanoganders.dk 'screen -rd todo'
