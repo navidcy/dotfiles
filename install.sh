5aa1cfd21ae6245f81d77b154b0606afe19dd4cf
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-09-06 14:17:34 +0200

    merged clone_oh_my_zsh.sh into install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,7 @@
 ./create_symlinks.sh
 
 # Clone oh-my-zsh
-./clone_oh_my_zsh.sh
-
-# Clone or update Vim repositories (with Pathogen)
-#cd .vim/bundle
-#./clone-repos.sh
-#./update-repos.sh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # Install Vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
