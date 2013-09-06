137f4deeac0fa50198dbbe08de0e9ff1c857af77
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-09-06 09:21:25 +0200

    install.sh modified

--- install.sh
+++ install.sh
@@ -4,8 +4,11 @@
 # Clone oh-my-zsh
 ./clone_oh_my_zsh.sh
 
+# Clone mutt solarized color scheme
+cd .mutt
+git clone https://github.com/altercation/mutt-colors-solarized.git
+
 # Clone or update Vim repositories
 cd .vim/bundle
 ./clone-repos.sh
 ./update-repos.sh
-
