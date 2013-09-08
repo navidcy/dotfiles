53a838b5176202f55b4393da892fbcc9e7979a98
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-09-08 13:08:15 +0200

    torifying wget command, added details on importing keys

--- bin/tor-browser-update.sh
+++ bin/tor-browser-update.sh
@@ -2,22 +2,29 @@
 
 # Optionally specify filename as command line argument
 
+# To import the signing key to Debian with:
+# $ gpg --keyserver keys.gnupg.net --recv 886DDD89
+# $ gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+# $ sudo apt-get update
+# $ sudo apt-get isntall deb.torproject.org-keyring
+
+DOWNLOADCMD=torify wget
+TORFOLDER=~/tor
+DIR="https://www.torproject.org/dist/torbrowser/linux/"
+
 if [[ -z "$1" ]]; then
     FILENAME="tor-browser-gnu-linux-x86_64-2.4.17-beta-1-dev-en-US"
 else 
     FILENAME=$1
 fi
 
-DIR="https://www.torproject.org/dist/torbrowser/linux/"
-
-TORFOLDER=~/tor
 cd $TORFOLDER &&\
 echo "Attempting to download Tor Browser Bundle and signature"
 TARGET=$DIR$FILENAME.tar.gz.asc
 echo $TARGET
-wget $TARGET &&\
+$DOWNLOADCMD $TARGET &&\
 TARGET=$DIR$FILENAME.tar.gz
 echo $TARGET
-wget $TARGET &&\
+$DOWNLOADCMD $TARGET &&\
 gpg --verify $FILENAME.tar.gz{.asc,} &&\
 tar xvfz $FILENAME.tar.gz
