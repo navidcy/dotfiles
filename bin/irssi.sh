ebfafc412016e289ca88f017ba06d081e536b164
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2013-10-27 21:39:23 +0100

    irssi script uses autossh

--- bin/irssi.sh
+++ bin/irssi.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #ssh -t -p 12344 ircuser@susanoganders.dk 'screen -rd irssi'
-ssh -t -p 12344 ircuser@susanoganders.dk 'bin/tmux-irssi.sh'
+#ssh -t -p 12344 ircuser@susanoganders.dk 'bin/tmux-irssi.sh'
+autossh -t -p 12344 ircuser@susanoganders.dk 'bin/tmux-irssi.sh'
