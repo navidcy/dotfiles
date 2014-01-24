35bdf49ea33b4efdb6b57346d7090abb885a31a2
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-01-24 11:17:12 +0100

    Caps as Ctrl, done with setxkbmap

--- bin/keyboard-layout-switch.sh
+++ bin/keyboard-layout-switch.sh
@@ -7,11 +7,15 @@ LAYOUTFILE="$HOME/.kbd-layout"
 LANG1="us"
 LANG2="dk"
 
+# make the caps lock key a ctrl key
+OPTS="-option ctrl:nocaps"
+
 # write the file if it doesn't exist
 if [ ! -f $LAYOUTFILE ]; then
     echo 'echo "us" > $LAYOUTFILE'
     echo "us" > $LAYOUTFILE
     setxkbmap $LANG1
+    xmodmap ~/.Xmodmap $OPTS
     exit
 fi
 
@@ -21,7 +25,9 @@ CURRLANG="`cat $LAYOUTFILE`"
 if [ "$CURRLANG" == "$LANG1" ]; then
     echo $LANG2 > $LAYOUTFILE
     setxkbmap $LANG2
+    xmodmap ~/.Xmodmap $OPTS
 else
     echo $LANG1 > $LAYOUTFILE
     setxkbmap $LANG1
+    xmodmap ~/.Xmodmap $OPTS
 fi
