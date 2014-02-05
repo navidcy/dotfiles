b338b535626b71502f93ba96f6065e648bc96098
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-02-05 13:27:45 +0100

    Modified install script to continue if a repo exists

--- bin/install_bspwm.sh
+++ bin/install_bspwm.sh
@@ -6,7 +6,7 @@
 # Debian dependencies
 sudo apt-get install xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev \
     libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev xfonts-terminus \
-    xfonts-artwiz hsetroot compton unclutter zathura
+    hsetroot compton unclutter zathura
 
 # Fonts: search for artwiz-aleczapka fonts, download from sourceforce.
 # Extract fonts to /home/adc/.fonts
@@ -20,21 +20,27 @@ FOLDER=~/code
 
 cd $FOLDER && \
 git clone git@github.com:baskerville/bspwm.git && \
-cd bspwm && make && sudo make install && cd .. && \
+cd bspwm && make && sudo make install
+
+cd $FOLDER && \
 git clone git@github.com:baskerville/sxhkd.git && \
-cd sxhkd && make && sudo make install && cd ..
+cd sxhkd && make && sudo make install
 
 # Panel
+cd $FOLDER && \
 git clone git@github.com:baskerville/sutils.git && \
-cd sutils && make && sudo make install && cd .. && \
+cd sutils && make && sudo make install
+
+cd $FOLDER && \
 git clone git@github.com:baskerville/xtitle.git && \
-cd xtitle && make && sudo make install && cd .. && \
+cd xtitle && make && sudo make install
+
+cd $FOLDER && \
 git clone git@github.com:LemonBoy/bar.git && \
-cd bar && make && sudo make install && cd ..
+cp ~/code/dotfiles/bar/config.h $FOLDER/bar/ &&
+cd bar && make && sudo make install
 
 # Panel configuration
 # Execute the following as root:
 # echo 'export PANEL_FIFO="/tmp/panel-fifo"' >> /etc/profile
 # echo 'export PANEL_FIFO="/tmp/panel-fifo"' >> /etc/zsh/zprofile
-
-#cp $FOLDER/bspwm/examples/panel/panel $FOLDER/bspwm/examples/panel/panel_bar ~/bin
