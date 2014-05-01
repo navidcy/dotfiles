007e3ff5ecf804d550205b2379a73eabf80a359f
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-05-01 09:31:53 +0200

    added compose mail hook

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -239,6 +239,26 @@
 ;; allow for updating mail using 'U' in the main view:
 (setq mu4e-get-mail-command "offlineimap")
 
+; Set the account for composing a message
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+    (if mu4e-compose-parent-message
+      (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+        (string-match "/\\(.*?\\)/" maildir)
+        (match-string 1 maildir))
+        (completing-read (format "Compose with account: (%s) "
+          (mapconcat #'(lambda (var) (car var)) my-mu4e-account-alist "/"))
+          (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+          nil t nil nil (caar my-mu4e-account-alist))))
+        (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+
 ;; enable inline images
 (setq mu4e-view-show-images t)
 ;; use imagemagick, if available
