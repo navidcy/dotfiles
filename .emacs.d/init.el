d2c215a180f2bf005ab677bdf031c638f39168c3
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-05-02 08:46:07 +0200

    removed custom templates as they didn't work

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -139,35 +139,39 @@
 
 ;; LaTeX templates, referenced with #+LaTeX_class: classname
 (require 'org-latex)
-(add-to-list 'org-export-latex-classes
-             '("myarticle"
-"\\documentclass[a4paper]{article}
-\\usepackage[utf8]{inputenc}
-\\usepackage{lmodern}
-\\usepackage[T1]{fontenc}
-\\newcommand\\foo{bar}
-               [NO-DEFAULT-PACKAGES]
-               [NO-PACKAGES]
-               [EXTRA]"
-            ("\\section{%s" . "\\section*{%s}")
-            ("\\subsection{%s" . "\\subsection*{%s}")
-            ("\\paragraph{%s" . "\\paragraph*{%s}")
-            ("\\subparagraph{%s" . "\\subparagraph*{%s}")))
-(add-to-list 'org-export-latex-classes
-             '("mybeaemer"
-"\\documentclass[presentation]{beamer}
-\\usepackage[utf8]{inputenc}
-\\usepackage{lmodern}
-\\usepackage[T1]{fontenc}
-               [NO-DEFAULT-PACKAGES]
-               [NO-PACKAGES]
-               [EXTRA]
-               [BEAMER-HEADER-EXTRA]"
-org-beamer-sectioning))
-
+;(add-to-list 'org-export-latex-classes
+;             '("myarticle"
+;"\\documentclass[a4paper]{article}
+;\\usepackage[utf8]{inputenc}
+;\\usepackage{lmodern}
+;\\usepackage[T1]{fontenc}
+;\\newcommand\\foo{bar}
+;               [NO-DEFAULT-PACKAGES]
+;               [NO-PACKAGES]
+;               [EXTRA]"
+;            ("\\section{%s" . "\\section*{%s}")
+;            ("\\subsection{%s" . "\\subsection*{%s}")
+;            ("\\paragraph{%s" . "\\paragraph*{%s}")
+;            ("\\subparagraph{%s" . "\\subparagraph*{%s}")))
+;(add-to-list 'org-export-latex-classes
+;             '("mybeaemer"
+;"\\documentclass[presentation]{beamer}
+;\\usepackage[utf8]{inputenc}
+;\\usepackage{lmodern}
+;\\usepackage[T1]{fontenc}
+;               [NO-DEFAULT-PACKAGES]
+;               [NO-PACKAGES]
+;               [EXTRA]
+;               [BEAMER-HEADER-EXTRA]"
+;org-beamer-sectioning))
 (setq reftex-default-bibliography '("~/owncloud/articles/adc-articles/BIBnew.bib"))
 
+;(setq org-latex-to-pdf-process (list "latexmk -pdf -bibtex %f"))
+;(setq org-latex-to-pdf-process (list "latexmk -pdf -bibtex %s"))
 ;(setq org-latex-pdf-process (quote ("texi2dvi --pdf --clean --verbose --batch %f" "bibtex %b" "texi2dvi --pdf --clean --verbose --batch %f" "texi2dvi --pdf --clean --verbose --batch %f")))
+
+;; render special characters using UTF-8 character set
+(setq org-pretty-entities t)
 
 ;; avoid compiz manager rendering bugs
 (add-to-list 'default-frame-alist '(alpha . 100))
