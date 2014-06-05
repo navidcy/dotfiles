7c7d21d6c3831fd54cbf4fba1736031e1bfe8655
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-06-05 14:42:54 +0200

    recent files with C-x C-r

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -85,7 +85,7 @@
 (require 'recentf)
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
-(global-set-key "\C-x \C-r" 'recentf-open-files)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
 ;; color theme
 (load-theme 'solarized-dark t)
