f948a3ada181283c3ec69ebd3eff9d239f1b5ac8
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-06-10 10:21:16 +0200

    remove mutt pane

--- bin/tmux-utility.sh
+++ bin/tmux-utility.sh
@@ -8,13 +8,12 @@ then
     # Create new session, name it, name the window, detach
     tmux new-session -s $SESSION -n misc -d
 
-    tmux send-keys -t $SESSION 'mutt' C-m   # process in first pane
+    #tmux send-keys -t $SESSION 'mutt' C-m   # process in first pane
+    tmux send-keys -t $SESSION 'vimpc' C-m   # process in first pane
     tmux split-window -h -p 60 -t $SESSION  # horizontal split
     #tmux split-window -v -p 10 -t $SESSION  # bottom right
     #tmux send-keys -t $SESSION 'htop' C-m   
     tmux select-pane -t $SESSION:1.1        # select first pane
-    tmux split-window -v -p 60 -t $SESSION  # create a pane below first pane
-    tmux send-keys -t $SESSION 'ncmpcpp' C-m  # process in second pane
     tmux split-window -v -p 66 -t $SESSION  # create a pane below second pane
     tmux send-keys -t $SESSION 'htop' C-m   # process in third pane
     tmux split-window -v -p 70 -t $SESSION  # create a pane below second pane
