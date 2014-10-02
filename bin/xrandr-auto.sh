22226dd2f8e02f261fc17f184404383368e83377
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2014-10-02 13:20:12 +0200

    added automatic display configuration

---
+++ bin/xrandr-auto.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LAPTOPSCR=eDP1
+EXTSCR=DP1
+#PROJECTOR=
+
+
+while true
+do
+
+    EXTSCRSTATUS=$(xrandr -q | grep "^$EXTSCR connected")
+    #PROJSTATUS=$(xrandr -q | grep "$EXTSCR connected")
+
+    echo $EXTSCRSTATUS
+
+    # Enable external display if connected
+    if [ -n "$EXTSCRSTATUS" ]; then  # if string is not empty
+        xrandr --output $EXTSCR --auto --output $LAPTOPSCR --off
+    else
+        xrandr --auto
+    fi
+
+    sleep 5
+
+done
