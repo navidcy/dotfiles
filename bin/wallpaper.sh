8d8985f6f195e371b058e92b1f63f784b10192e8
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-03-24 16:50:48 +0100

    call wallpaper script with xrandr

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-hsetroot -fill ~/wallpapers/fall.jpg # single screen
+#hsetroot -fill ~/wallpapers/fall.jpg # single screen
+hsetroot -full ~/wallpapers/hackingtheloop_1920.jpg # single screen
