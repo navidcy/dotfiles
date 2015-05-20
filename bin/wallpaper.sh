7da86d6cbad7dc3b40622434cdbb4ff7a10de39f
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-05-20 12:57:32 +0200

    change wallpaper

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #hsetroot -fill ~/wallpapers/fall.jpg # single screen
-hsetroot -full ~/wallpapers/hackingtheloop_1920.jpg # single screen
+#hsetroot -full ~/wallpapers/hackingtheloop_1920.jpg # single screen
+hsetroot -fill ~/wallpapers/polygon-art-17498.jpg
