8723cc252fd0adf422d35a1a1152c1754efeccea
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2015-05-20 12:56:00 +0200

    do not symlink .emacs.d

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -16,7 +16,7 @@ for F in .bashrc .vimrc .inputrc .tmux.conf .xpdfrc .signature .Xresources \
 done
 
 # Home folder dotfolders
-for F in .colors .config/uzbl .config/awesome .config/dwb .config/bspwm .config/sxhkd .config/luakit .config/openbox .config/zathura .tmuxinator .i3 .mutt .ncmpcpp .vim .vimperrator .w3m wallpapers .emacs.d; do
+for F in .colors .config/uzbl .config/awesome .config/dwb .config/bspwm .config/sxhkd .config/luakit .config/openbox .config/zathura .tmuxinator .i3 .mutt .ncmpcpp .vim .vimperrator .w3m wallpapers; do
   SOURCE=$PWD/$F
   TARGET=~/$F
 
