56fe5351ab3b12e225deef8a5969df356782d682
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2016-03-07 14:01:25 -0800

    add msmtp with gnome keyring functionality

--- bin/install_debian_pkgs.sh
+++ bin/install_debian_pkgs.sh
@@ -3,8 +3,8 @@ set -e
 sudo apt-get update
 
 sudo apt-get install vim tmux git zsh i3 exuberant-ctags haskell-platform \
-    build-essential ruby mutt offlineimap msmtp autossh tor xbindkeys \
-    xautolock pass urlview elinks redshift libnotify-bin \
+    build-essential ruby mutt offlineimap msmtp msmtp-gnome autossh tor \
+    xbindkeys xautolock pass urlview elinks redshift libnotify-bin \
     hsetroot xbacklight i3lock scrot imagemagick mpc htop \
     rxvt-unicode-256color texlive cmake sshfs paraview python-numpy \
     python-scipy python-matplotlib ipython mplayer zathura okular python-pip \
