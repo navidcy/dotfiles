00b5bf440ead309d8ad093cc20af3d25ad08b43b
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2016-03-07 15:28:46 -0800

    add mpd and clients

--- bin/install_debian_pkgs.sh
+++ bin/install_debian_pkgs.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install vim tmux git zsh i3 exuberant-ctags haskell-platform \
     build-essential ruby mutt offlineimap msmtp msmtp-gnome autossh tor \
     xbindkeys xautolock pass urlview elinks redshift libnotify-bin \
-    hsetroot xbacklight i3lock scrot imagemagick mpc htop \
+    hsetroot xbacklight i3lock scrot imagemagick mpd vimpc ncmpcpp mpc htop \
     rxvt-unicode-256color texlive cmake sshfs paraview python-numpy \
     python-scipy python-matplotlib ipython mplayer zathura okular python-pip \
     dunst latexmk texlive-full screenfetch i3blocks torbrowser-launcher vim-nox
