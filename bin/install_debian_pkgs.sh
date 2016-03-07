a4c37594989b3733cfcc585efd9ef5338027c1a2
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2016-03-07 10:48:05 -0800

    remove owncloud client

--- bin/install_debian_pkgs.sh
+++ bin/install_debian_pkgs.sh
@@ -4,7 +4,7 @@ apt-get update
 
 apt-get install vim tmux git zsh i3 exuberant-ctags haskell-platform \
     build-essential ruby mutt offlineimap msmtp autossh tor xbindkeys \
-    xautolock pass urlview elinks redshift libnotify-bin owncloud-client \
+    xautolock pass urlview elinks redshift libnotify-bin \
     hsetroot xbacklight i3lock scrot imagemagick mpc htop \
     rxvt-unicode-256color texlive cmake sshfs paraview python-numpy \
     python-scipy python-matplotlib ipython mplayer zathura okular python-pip \
