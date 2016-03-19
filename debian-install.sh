57da98843a61e72ec749e69ad2cd9f01b59d83ed
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2016-03-19 08:25:43 -0700

    sort package list, add mosh

--- debian-install.sh
+++ debian-install.sh
@@ -2,14 +2,65 @@
 set -e
 sudo apt-get update
 
-sudo apt-get install vim tmux git zsh i3 exuberant-ctags haskell-platform \
-    build-essential ruby mutt offlineimap msmtp msmtp-gnome autossh tor \
-    xbindkeys xautolock pass urlview elinks redshift libnotify-bin \
-    hsetroot xbacklight i3lock scrot imagemagick mpd ncmpcpp mpc htop \
-    rxvt-unicode-256color texlive cmake sshfs paraview python-numpy \
-    python-scipy python-matplotlib ipython mplayer zathura okular python-pip \
-    dunst latexmk texlive-full screenfetch i3blocks torbrowser-launcher \
-    vim-nox w3m imapfilter bogofilter tig ntp youtube-dl
+sudo apt-get install \
+    autossh \
+    bogofilter \
+    build-essential \
+    cmake \
+    dunst \
+    elinks \
+    exuberant-ctags \
+    git \
+    haskell-platform \
+    hsetroot \
+    htop \
+    i3 \
+    i3blocks \
+    i3lock \
+    imagemagick \
+    imapfilter \
+    ipython \
+    latexmk \
+    libnotify-bin \
+    mpc \
+    mpd \
+    mplayer \
+    msmtp \
+    msmtp-gnome \
+    mutt \
+    ncmpcpp \
+    ntp \
+    offlineimap \
+    okular \
+    paraview \
+    pass \
+    python-matplotlib \
+    python-numpy \
+    python-pip \
+    python-scipy \
+    redshift \
+    ruby \
+    rxvt-unicode-256color \
+    screenfetch \
+    scrot \
+    sshfs \
+    texlive \
+    texlive-full \
+    tig \
+    tmux \
+    tor \
+    torbrowser-launcher \
+    urlview \
+    vim \
+    vim-nox \
+    w3m \
+    xautolock \
+    xbacklight \
+    xbindkeys \
+    youtube-dl
+    zathura \
+    zsh \
+
 
 sudo gem install tmuxinator
 
