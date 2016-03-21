8ad0c783fe28793073d3a8f487e7cf127b997261
Anders Damsgaard <anders.damsgaard@geo.au.dk>
2016-03-21 12:47:36 -0700

    fix wallpaper folder path

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -2,4 +2,4 @@
 #hsetroot -fill ~/wallpapers/fall.jpg # single screen
 #hsetroot -full ~/wallpapers/hackingtheloop_1920.jpg # single screen
 #hsetroot -fill ~/wallpapers/polygon-art-17498.jpg
-hsetroot -fill ~/wallpapers/current_wallpapers/overdeepenings-Edit-Edit.jpg
+hsetroot -fill ~/photos/wallpapers/current_wallpapers/overdeepenings-Edit-Edit.jpg
