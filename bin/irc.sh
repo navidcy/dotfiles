2c0836cd700581190ac4e796a88d272eb6d0ae4b
Anders Damsgaard Christensen <adc@geo.au.dk>
2016-05-20 13:16:53 -0700

    change irc script to point to new server

--- bin/irc.sh
+++ bin/irc.sh
@@ -8,13 +8,14 @@ HOST=adamsgaard.dk
 #PORT=12344
 PORT=22
 #POSTCMD=\'bin/tmux-irc.sh\'
-POSTCMD=\'bin/tmux-irc.sh\'
+POSTCMD=
 
 UNAMESTR=`uname`
 if [[ "$UNAMESTR" == 'Linux' ]]; then
     autossh -t -p $PORT $USER@$HOST $POSTCMD
 elif [[ "$UNAMESTR" == 'Darwin' ]]; then
-    ssh -t -p $PORT $USER@$HOST $POSTCMD
+    #ssh -t -p $PORT $USER@$HOST $POSTCMD
+    mosh $USER@$HOST $POSTCMD
 else
     echo "Platform not supported"
     exit 1
