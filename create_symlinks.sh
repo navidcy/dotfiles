8e7eb22a7932505d278239c812e012c27994fbae
Anders Damsgaard Christensen <adc@geo.au.dk>
2016-06-06 09:47:34 -0700

    create read-only cache folder

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -57,3 +57,7 @@ mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
 ln -s ~/.vim $XDG_CONFIG_HOME/nvim
 ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 cd -
+
+# Mutt cache folder
+mkdir -p ~/.mutt/cache
+chmod 0700 ~/.mutt/cache
