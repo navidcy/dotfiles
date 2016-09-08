9f13ed9bf70737e330357f7136ffc01f4cef6f77
Anders Damsgaard Christensen <adc@geo.au.dk>
2016-09-08 10:32:51 -0700

    add metadata to output file

--- bin/youtube-dl-music.sh
+++ bin/youtube-dl-music.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
-youtube-dl \
+read -p "torify? [Y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Nn]$ ]]; then
+    prefix="torify"
+fi
+
+$prefix youtube-dl \
+    --format bestaudio \
+    --extract-audio \
+    --audio-quality 0 \
+    --audio-format mp3 \
+    $1
+
+read -p "add metadata? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+
+    filename=$(youtube-dl \
+        --get-filename \
         --format bestaudio \
         --extract-audio \
         --audio-quality 0 \
         --audio-format mp3 \
-        $1
+        $1)
+
+    read -p "artist: " artist
+    read -p "album: " album
+    read -p "song: " song
+
+    id3v2 --artist "$artist" --album "$album" --song "$song" $filename
+fi
