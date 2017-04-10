6513f1f0d43fb9a8871a0b3b620afededdf44f94
Anders Damsgaard <andersd@riseup.net>
2017-04-10 13:04:34 -0400

    improve ssh connect scripts

--- bin/google-cloud-lamps-instance1-ssh.sh
+++ bin/google-cloud-lamps-instance1-ssh.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-remotecommand='tmux attach -dt 0'
 set -e
+remotecommand='tmux attach -dt 0'
+host=104.154.231.211
+user=andersdc
 if [ "$1" = "-s" ]; then
-    ssh -i ~/.ssh/google-cloud-ssh-key andersdc@104.154.231.211 \
+    ssh -i ~/.ssh/google-cloud-ssh-key $user@$host \
         -t $remotecommand
 elif [ "$1" = "-a" ]; then
-    autossh -i ~/.ssh/google-cloud-ssh-key andersdc@104.154.231.211
+    autossh -i ~/.ssh/google-cloud-ssh-key $user@$host
 else
-    mosh --ssh="ssh -i ~/.ssh/google-cloud-ssh-key" andersdc@104.154.231.211 \
+    mosh --ssh="ssh -i ~/.ssh/google-cloud-ssh-key" $user@$host \
         -- $remotecommand
 fi
