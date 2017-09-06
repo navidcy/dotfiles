c862c464e3f433bdb1fa88ee88371935c5e04ff7
Anders Damsgaard <andersd@riseup.net>
2017-09-06 12:47:37 -0400

    change ssh key

--- bin/google-cloud-lamps-instance1-ssh.sh
+++ bin/google-cloud-lamps-instance1-ssh.sh
@@ -4,14 +4,12 @@ remotecommand='tmux attach -dt 0'
 host=104.154.231.211
 user=andersdc
 if [ "$1" = "-s" ]; then
-    ssh -i ~/.ssh/google-cloud-ssh-key $user@$host \
-        -t $remotecommand
+    ssh $user@$host -t $remotecommand
 elif [ "$1" = "-a" ]; then
-    autossh -i ~/.ssh/google-cloud-ssh-key $user@$host
+    autossh $user@$host
 else
     if [[ "$1" = "notmux" ]]; then
         remotecommand=""
     fi
-    mosh --ssh="ssh -i ~/.ssh/google-cloud-ssh-key" $user@$host \
-        -- $remotecommand
+    mosh $user@$host -- $remotecommand
 fi
