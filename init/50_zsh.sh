717d47d217207445ca9869699e8f4eddce3e5be1
Anders Damsgaard <andersd@riseup.net>
2017-09-27 17:27:11 -0400

    change file permissions and add zsh script

---
+++ init/50_zsh.sh
@@ -0,0 +1,8 @@
+# Clone zsh syntax highlighting and git prompt
+(mkdir -p $HOME/code && cd $HOME/code &&
+if [ ! -d zsh-syntax-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+fi
+if [ ! -d zsh-git-prompt ]; then
+    git clone https://github.com/olivierverdier/zsh-git-prompt.git 
+fi)
