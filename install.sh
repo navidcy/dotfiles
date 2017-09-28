daea1973037f6d26e97c2944602b17495e95b92e
Anders Damsgaard <andersd@riseup.net>
2017-09-28 13:27:49 -0400

    do not ask to install individual casks. remove legacy code

--- install.sh
+++ install.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
-#set -e
 
 # Create symlinks
 ./create_symlinks.sh
 
-# Clone zsh syntax highlighting and git prompt
-mkdir -p $HOME/code && cd $HOME/code && \
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/olivierverdier/zsh-git-prompt.git
-
-cd ~/bin && curl -OL https://raw.github.com/richo/battery/master/bin/battery &&\
-    chmod +x ~/bin/battery
-
-# Install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install Python GDB configuration
-curl -OL git.io/.gdbinit
+# Run initialization scripts
+for s in init/*.sh; do
+  bash $s;
+done
