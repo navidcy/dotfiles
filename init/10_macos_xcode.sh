60a66f79df9feb2e30d0afe540d1fafc0cee56ad
Anders Damsgaard <andersd@riseup.net>
2017-10-02 22:32:17 -0400

    double verification of command line tools status

--- init/10_macos_xcode.sh
+++ init/10_macos_xcode.sh
@@ -3,6 +3,12 @@
 # OSX-stuff only
 [[ "$(uname)" != "Darwin" ]] && return 1
 
-if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-    sudo xcode-select -switch /usr/bin
+# check if command line tools are installed
+xcode-select --print-path >/dev/null 2>&1
+xcodestatus=$?
+gcc --version >/dev/null 2>&1
+gccstatus=$?
+if [[ $xcodestatus -ne 0 || $gccstatus -ne 0 ]]; then
+    echo "xcode status: $xcodestatus, gcc status: $gccstatus"
+    sudo xcode-select --install
 fi
