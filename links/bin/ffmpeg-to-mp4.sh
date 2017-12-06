ba352c5712769467707cc2fdef1b366d34ba252f
Anders Damsgaard <andersd@riseup.net>
2017-12-06 15:27:02 -0500

    Improve file renaming

--- links/bin/ffmpeg-to-mp4.sh
+++ links/bin/ffmpeg-to-mp4.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-output=$(echo $1 | sed 's/\..*$/.mp4/')
+#output=$(echo $1 | sed 's/\..*$/.mp4/')
+output=${1%.*}.mp4
+
 ffmpeg -i $1 \
     -acodec libfaac -b:a 128k -vcodec mpeg4 -b:v 1200k -flags +aic+mv4 \
     $output
+
+# constant bitrate:
+#ffmpeg -i $1 -vcodec libx264 -crf 20 $output
+
 echo $output
+
