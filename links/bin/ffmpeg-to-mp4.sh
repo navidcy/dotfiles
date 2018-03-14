cec6bc559959dee980554d606538b8819a141e60
Anders Damsgaard <andersd@riseup.net>
2018-03-14 15:33:36 -0400

    Overwrite ffmpeg output files without asking

--- links/bin/ffmpeg-to-mp4.sh
+++ links/bin/ffmpeg-to-mp4.sh
@@ -12,7 +12,7 @@ output="${1%.*}.mp4"
 ffmpeg -i "$1" \
     -vf scale="trunc(iw/2)*2:trunc(ih/2)*2" \
     -c:v libx264 -profile:v high -pix_fmt yuv420p \
-    -g 30 -r 30 \
+    -g 30 -r 30 -y \
     "$output"
 
 #ffmpeg -i movie.avi -sameq -vcodec libx264 -x264opts keyint=25 \
@@ -22,7 +22,7 @@ ffmpeg -i "$1" \
 #ffmpeg -i $1 -vcodec libx264 -crf 20 $output
 
 # Output first frame as an image
-ffmpeg -i "$1" -vframes 1 -f image2 "${output%.*}.png"
+ffmpeg -i "$1" -y -vframes 1 -f image2 "${output%.*}.png"
 
 # Output last frame as an image
 #lastframe_index=$(ffprobe -show_streams "$1" 2> /dev/null | \
