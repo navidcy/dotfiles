3a0933002fa22770b20f04eed20ecf841e514789
Anders Damsgaard <anders@adamsgaard.dk>
2018-04-12 10:34:26 -0400

    Fix synchronization script to idkfa

--- links/bin/sync-music-idkfa.sh
+++ links/bin/sync-music-idkfa.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 host=ad@idkfa.ucsd.edu:/home/ad/music/
+musicdir="$HOME/Music/iTunes/iTunes Media/Music/"
 
 echo "syncing new music from $host"
-rsync -rav --progress -e "ssh" $host /Users/ad/Music/iTunes/Music/
+rsync -rav --progress -e "ssh" $host "$musicdir"
 
 echo "syncing new music to $host"
-rsync -rav --progress -e "ssh" /Users/ad/Music/iTunes/Music/ $host
+rsync -rav --progress -e "ssh" "$musicdir" $host
