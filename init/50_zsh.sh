506af47885c2855f356abd270a552c105884963d
Anders Damsgaard <anders@adamsgaard.dk>
2018-05-30 14:43:26 -0400

    Install zplug, not zgen

--- init/50_zsh.sh
+++ init/50_zsh.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
 # zsh plugin manager
-# (mkdir -p ~/code && cd ~/code &&
-#     if [ ! -d zplug ]; then
-#         git clone https://github.com/zplug/zplug
-#     fi
-#     )
-
-if [ ! -d ~/.zgen ]; then
-    git clone https://github.com/tarjoilija/zgen ~/.zgen
-fi
+(mkdir -p ~/code && cd ~/code &&
+    if [ ! -d zplug ]; then
+        git clone https://github.com/zplug/zplug
+    fi
+    )
